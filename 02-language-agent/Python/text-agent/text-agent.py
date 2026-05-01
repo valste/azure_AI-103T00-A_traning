b46e3ae70c8fa27f60d2afd9284bfb589a88fcc8
valste <valerian.stenske@gmail.com>
2026-05-01 11:49:19 +0200

    completed azure language with mcp 02-language-agent

---
+++ 02-language-agent/Python/text-agent/text-agent.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import os
+
+# import namespaces
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+
+
+def main():
+    try:
+        # Clear the console
+        os.system("cls" if os.name == "nt" else "clear")
+
+        # Get Configuration Settings
+        load_dotenv(override=True)
+        foundry_endpoint = os.getenv("FOUNDRY_ENDPOINT")
+        agent_name = os.getenv("AGENT_NAME")
+
+        # Get project client
+        project_client = AIProjectClient(
+            endpoint=foundry_endpoint,
+            credential=DefaultAzureCredential(),
+        )
+
+        # Get an OpenAI client
+        openai_client = project_client.get_openai_client()
+
+        # Use the agent to get a response
+        """Extract named entities from the following text: "Pierre and I went to Paris on July 14th."""
+
+        prompt = input("User prompt: ")
+        response = openai_client.responses.create(
+            input=[{"role": "user", "content": prompt}],
+            extra_body={
+                "agent_reference": {"name": agent_name, "type": "agent_reference"}
+            },
+        )
+
+        print(f"{agent_name}: {response.output_text}")
+
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
